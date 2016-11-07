89c0478b7808e540ecc30baa09109e5c65b538e9
Jia Xun <chaijiaxun@gmail.com>
2016-11-06 23:10:34 -0800

    Added pictures to the stores

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ sweetApp.run(function($rootScope, $location, dataService) {
     $rootScope.DataService = function() {
       return dataService;
     }
-
     $rootScope.logout = function() {
       dataService.logout();
       $location.path('/');
--- js/database/stores.js
+++ js/database/stores.js
@@ -3,114 +3,107 @@ function generateStoreData() {
   [
     {
       name: "Super Bakery",
-      img: "http://placehold.it/300x200",
       location: "123 Some street",
       lat: 1.2949889884722228,
       lon: 103.85204315185548
     },
     {
       name: "Super Bakery",
-      img: "http://placehold.it/300x200",
       location: "123 Some street",
       lat: 1.304599552047222,
       lon: 103.8604116439819
     },
     {
       name: "Super Bakery",
-      img: "http://placehold.it/300x200",
       location: "123 Some street",
       lat: 1.3154113921738335,
       lon: 103.85757923126225
     },
     {
       name: "Super Bakery",
-      img: "http://placehold.it/300x200",
       location: "123 Some street",
       lat: 1.3137810383194464,
       lon: 103.87830734252934
     },
     {
       name: "Super Bakery",
-      img: "http://placehold.it/300x200",
       location: "123 Some street",
       lat: 1.287008760656557,
       lon: 103.8405418395996
     },
     {
       name: "Super Bakery",
-      img: "http://placehold.it/300x200",
       location: "123 Some street",
       lat: 1.3065731453895093,
       lon: 103.87762069702143
     },
     {
       name: "Super Bakery",
-      img: "http://placehold.it/300x200",
       location: "123 Some street",
       lat: 1.312751340599998,
       lon: 103.8470649719238
     },
     {
       name: "Super Bakery",
-      img: "http://placehold.it/300x200",
       location: "123 Some street",
       lat: 1.3378071972557988,
       lon: 103.85187149047858
     },
     {
       name: "Super Bakery",
-      img: "http://placehold.it/300x200",
       location: "123 Some street",
       lat: 1.3319722944135324,
       lon: 103.84981155395504
     },
     {
       name: "Super Bakery",
-      img: "http://placehold.it/300x200",
       location: "123 Some street",
       lat: 1.3182430568620136,
       lon: 103.86938095092776
     },
     {
       name: "Super Bakery",
-      img: "http://placehold.it/300x200",
       location: "123 Some street",
       lat: 1.339180113557158,
       lon: 103.8745307922363
     },
     {
       name: "Super Bakery",
-      img: "http://placehold.it/300x200",
       location: "123 Some street",
       lat: 1.297649058834867,
       lon: 103.8541030883789
     },
     {
       name: "Super Bakery",
-      img: "http://placehold.it/300x200",
       location: "123 Some street",
       lat: 1.3023685316222549,
       lon: 103.87809276580819
     },
     {
       name: "Super Bakery",
-      img: "http://placehold.it/300x200",
       location: "123 Some street",
       lat: 1.294388327036012,
       lon: 103.8689947128295
     },
     {
       name: "Super Bakery",
-      img: "http://placehold.it/300x200",
       location: "123 Some street",
       lat: 1.2997942748317888,
       lon: 103.86247158050538
     }
   ];
+
+  function pad(n, width, z) {
+    z = z || '0';
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+  }
+
   var i = 0;
   _.each ( stores, function (store) {
     store.id = i;
     i++;
+    store.img = window.baseURL + "img/stores/store-"+pad(i,2)+".jpg";
   });
 
   return stores;
